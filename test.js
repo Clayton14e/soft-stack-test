0956df978153864ad8ac78fc3b0ad20f0d373098
Clayton <clayton.fortini@gmail.com>
2019-10-28 22:57:46 -0400

    Update 1

---
+++ test.js
@@ -0,0 +1,10 @@
+// Test Script for Git Hub Testing
+let xPositions = [];
+let yPositions = [];
+
+for(let i = 0; i < 100; i++){
+    xPositions[i] = random(0, 400);
+    yPositions[i] = random(0, 400);
+}
+
+console.log(xPositions);
